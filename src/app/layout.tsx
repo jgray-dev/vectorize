4f7a3c1359a8d78881c8cb8ef38c191c675e542b
Jackson Gray <nohaxjustdoge@gmail.com>
2024-06-16 21:45:00 +0000

    Update layout with NavBar component and dark background

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "~/styles/globals.css";
 
 import { GeistSans } from "geist/font/sans";
+import NavBar from "~/app/_components/NavBar";
 
 export const metadata = {
   title: "Create T3 App",
@@ -15,7 +16,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
-      <body>{children}</body>
+      <body className={"bg-black/80 text-white"}>
+      <NavBar />
+      {children}
+      </body>
     </html>
   );
 }
