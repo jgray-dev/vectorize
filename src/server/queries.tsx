e43141537dc5ec6c681266ee57576c1436044bb8
Jackson Gray <nohaxjustdoge@gmail.com>
2024-06-25 00:20:19 +0000

    Refactor: Switch from Voyage AI to OpenAI for embeddings

--- src/server/queries.tsx
+++ src/server/queries.tsx
@@ -1,56 +1,33 @@
 "use server";
 
-//Interfaces
 
-// Embeddings interfaces
-interface Embedding {
-  object: string;
-  embedding: number[];
-  index: number;
-}
+import { embed } from "ai";
 
-interface Usage {
-  total_tokens: number;
-}
 
-export interface Response {
-  object: string;
-  data: Embedding[];
-  model: string;
-  usage: Usage;
-}
 
-export async function getEmbedding(
-  text: string,
-  tags?: string,
-): Promise<number[]> {
-  const apiKey = process.env.VOYAGE_API_KEY;
-  console.log("API Key:", apiKey);
+import OpenAI from "openai";
 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-call
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI,
+});
+
+
+export async function getEmbedding(text: string): Promise<number[]> {
   try {
-    const response = await fetch("https://api.voyageai.com/v1/embeddings", {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${apiKey}`,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        input: `${text} ${tags}`,
-        model: "voyage-large-2",
-      }),
+    const embedding = await openai.embeddings.create({
+      model: "text-embedding-3-small",
+      input: text,
+      encoding_format: "float",
     });
-
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    const data: Response = await response.json();
-    console.log(data);
-    if (data.data[0]?.embedding) {
-      return data.data[0].embedding;
+    if (embedding.data[0]?.embedding) {
+      return embedding.data[0].embedding;
     } else {
       return [];
     }
-  } catch (error) {
-    console.error("Error:", error);
-    return [];
+  } catch (e) {
+    console.error(e);
+    return [0];
   }
 }
 
@@ -62,12 +39,7 @@ export async function vectorToString(vector: number[]): Promise<string> {
   return string;
 }
 
-import OpenAI from "openai";
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-call
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI,
-});
 
 export async function callOpenAI(text: string, system: string) {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-assignment
@@ -77,10 +49,8 @@ export async function callOpenAI(text: string, system: string) {
       { role: "user", content: `${text}` },
     ],
     model: "gpt-4-turbo",
-  });
+  }); 
   // @ts-expect-error fts
   // eslint-disable-next-line @typescript-eslint/no-unsafe-argument,@typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-return
   return completion.choices[0].message.content
 }
-
-
